a955b71c17ce986214302859a83ce81b73e0753e
NthiCreate <shanthaswetha1@gmail.com>
2025-08-10 18:06:51 +0530

    folder structure added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
 	name = 'Generative AI Project',
 	version = '0.0.0',
-	author ='Nithi',
-	author_email = 'nithiyadharshini29@gmail.com',
+	author ='NithiCreate',
+	author_email = 'shantahswetha1@gamil.com',
 	packages = find_packages(),
 	install_requires = []
 )
